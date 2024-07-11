06fcbc3746bd3177682a304a8598734815e0b790
aledraken <aledraken@proton.me>
2024-07-11 17:37:00 +0200

    Adobe cleaner script for not so legit copies of adobe products

---
+++ Scripts/Adobe cleaner.bat
@@ -0,0 +1,11 @@
+del /q /f "C:\Program Files (x86)\Adobe\Adobe Sync\CoreSync\CoreSync.exe"
+
+del /q /f "C:\Program Files\Adobe\Adobe Creative Cloud Experience\CCXProcess.exe"
+
+del /q /f "C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\ADS\Adobe Desktop Service.exe"
+
+del /q /f "C:\Program Files\Common Files\Adobe\Creative Cloud Libraries\CCLibrary.exe"
+
+# Change the name with new version
+
+del /q /f "C:\Program Files\Adobe\Adobe Photoshop 2024\LogTransport2.exe.bak"
